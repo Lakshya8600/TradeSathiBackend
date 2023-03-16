60a2ab0e61b4b93070c1aecef79b00a5a9f6b5b9
Lakshya Kumrawat <73424857+Lakshya8600@users.noreply.github.com>
2023-03-16 11:13:53 +0530

    ui
    gj

--- app.py
+++ app.py
@@ -2,51 +2,95 @@ from flask import Flask, redirect, url_for, request , jsonify
 from flask_sqlalchemy import SQLAlchemy
 import yfinance as yf, threading , time , pandas as pd , os
 from datetime import date
+import pandas_ta as ta
+from datetime import datetime
+from flask_cors import CORS
+
 app = Flask(__name__ , template_folder='templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sudden.db'
-dbsudden = SQLAlchemy(app) 
+cors = CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+# app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
 
 CompanyNames = pd.read_csv('StockData/CompanyNames.csv')
 CompanyDataList = os.listdir(os.getcwd()+"/StockData") 
+suddenpt7list = []
+sudden2perlist = []
+vwaplist = []
+rsi30list = []
+rsi70list = []
 
-class User(dbsudden.Model):
-   id = dbsudden.Column(dbsudden.Integer, primary_key=True)
-   Stockname = dbsudden.Column(dbsudden.String(30), nullable=False)
-   # percentage = dbsudden.Column(dbsudden.Integer, nullable=False)
-   Minute = dbsudden.Column(dbsudden.String(20), nullable=False)
-   
-   def __repr__(self):
-      return '<User %r>' % self.username
+def lstostr(list):
+   strii = ""
+   for names in list:
+      strii = strii + '#' + names
+   return str(strii)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Minute = db.Column(db.Integer)
+    suddenpt7 = db.Column(db.String(1500))
+    sudden2per = db.Column(db.String(1500))
+    vwap = db.Column(db.String(1500))
+    rsi30 = db.Column(db.String(1500))
+    rsi70 = db.Column(db.String(1500))
+   #  volup = db.Column(db.String(1500), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.Minute
 
 def bgtask():
    """background task of getting yfinance data and storing in data base with formatting"""
+   currentMin = datetime.now().minute
    while True:
-      # searching for all the companies in the companynames
-      for Company in CompanyNames.iloc[:,0]:
-         # getting stock prices data and getting the 
-         try: 
-            data = yf.download(tickers=Company,start=date.today(), interval='1m')
-            Timeopen = data.iloc[-10,0]
-            Timeclose = data.iloc[-1,3]
-         except:
-            pass
+      if currentMin is not datetime.now().minute:
+         currentMin = datetime.now().minute
+         for Company in CompanyNames.iloc[:,0]:
+            try: 
+               data = yf.download(tickers=Company,start=date.today(), interval='1m')
+               vwapdatalist = ta.vwap(data.iloc[:,1],data.iloc[:,2],data.iloc[:,3],data.iloc[:,5])
+               rsidatalist = ta.rsi(data.iloc[:,3]) 
+               Timeopen = data.iloc[-10,0]
+               Timeclose = data.iloc[-1,3]
+
+               percentchange = 0
+               if Timeopen >= Timeclose:
+                  percentchange = ((Timeopen-Timeclose)/Timeclose)*100
+               else:
+                  percentchange = ((Timeclose-Timeopen)/Timeopen)*100
+
+               if 2 > percentchange >= 0.7:
+                  suddenpt7list.append(Company)
+               if percentchange >= 2:
+                  sudden2perlist.append(Company)
+               if vwapdatalist.iloc[-1,0]+(vwapdatalist.iloc[-1,0]*(0.05/100)) >= Timeclose >= vwapdatalist.iloc[-1,0]-(vwapdatalist.iloc[-1,0]*(0.05/100)):
+                  vwaplist.append(Company)
+               if rsidatalist.iloc[-1,0] < 30:
+                  rsi30list.append(Company)
+               if rsidatalist.iloc[-1,0] > 70:
+                  rsi70list.append(Company)
+
+            except:
+               pass
+
+         with app.app_context():
+            databaserow = User(Minute=datetime.now().minute, suddenpt7=lstostr(suddenpt7list), sudden2per=lstostr(sudden2perlist), vwap=lstostr(vwaplist), rsi30=lstostr(rsi30list), rsi70=lstostr(rsi70list))
+            db.session.add(databaserow)
+            db.session.commit()
          
-         percentchange = 0
-         if Timeopen >= Timeclose:
-            percentchange = ((Timeopen-Timeclose)/Timeclose)*100
-         else:
-            percentchange = ((Timeclose-Timeopen)/Timeopen)*100
 
-         if percentchange > 0.5:
-            print(Company)
-            print(percentchange)
-            
-
-@app.route('/suddench',methods = ['POST', 'GET'])
+@app.route('/getinfo',methods = ['POST', 'GET'])
 def hello_world():
    if request.method == 'GET':
-      return jsonify( {"message": "hellllldsflkjp", "severity": "danger"})
+      with app.app_context():
+         returndata = User.query.all()
+         # print(returndata[-1].Minute)
+      
+      return jsonify( {"Minute": returndata[-1].Minute , "suddenpt7": returndata[-1].suddenpt7,"sudden2per": returndata[-1].sudden2per, "vwap": returndata[-1].vwap, "rsi30": returndata[-1].rsi30, "rsi70": returndata[-1].rsi70})
 
-if __name__ == '__main__':  
-   threading.Thread(target=bgtask).start()
-   app.run(debug=True)
+if __name__ == '__main__':    
+   with app.app_context():
+      threading.Thread(target=bgtask).start()
+      app.run(debug=True)
--- rough.py
+++ rough.py
@@ -1,4 +1,10 @@
+import pandas_ta as ta
 import yfinance as yf 
 from datetime import date
 data = yf.download(tickers="HDFC.NS",start=date.today(), interval='1m')
 print(data)
+# h = ta.vwap(data.iloc[:,1],data.iloc[:,2],data.iloc[:,3],data.iloc[:,5])
+h = ta.rsi(data.iloc[:,3])
+print(h)
+
+
